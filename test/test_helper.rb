5283c2d5150875046ae3cc7c2f35482731d8dcbd
H.Matsutani <matsutani.hidehisa@gmail.com>
2025-12-27 23:36:17 +0900

    change test_helper.rb -- add switching feature TEST_CACHE

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,18 @@
 
 require File.expand_path('../../../test/test_helper', __dir__)
 require_relative 'authenticate_user'
+
+# Ensure tests use a deterministic cache. Use TEST_CACHE=redis to test
+# multi-process shared cache behavior with Redis (set REDIS_URL accordingly).
+begin
+	if defined?(Rails) && Rails.respond_to?(:cache)
+		if ENV['TEST_CACHE'] == 'redis'
+			redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/1'
+			Rails.cache = ActiveSupport::Cache::RedisCacheStore.new(url: redis_url)
+		else
+			Rails.cache = ActiveSupport::Cache::MemoryStore.new
+		end
+	end
+rescue StandardError
+	# ignore if cache cannot be set here
+end
