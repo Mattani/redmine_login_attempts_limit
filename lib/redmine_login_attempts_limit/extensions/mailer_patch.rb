bf5bf97f88ae31523c3a5c08b80cad91ed84161a
H.Matsutani <matsutani.hidehisa@gmail.com>
2026-01-09 14:53:25 +0900

    fix syntax error

--- lib/redmine_login_attempts_limit/extensions/mailer_patch.rb
+++ lib/redmine_login_attempts_limit/extensions/mailer_patch.rb
@@ -5,34 +5,33 @@
 # user is blocked.
 #
 module RedmineLoginAttemptsLimit::Extensions::MailerPatch
-      def self.included(base)
-        base.extend(ClassMethods)
-        base.include(InstanceMethods)
-      end
+  def self.included(base)
+    base.extend(ClassMethods)
+    base.include(InstanceMethods)
+  end
 
-      ##
-      # Injects ClassMethods into base class.
-      #
-      module ClassMethods
-        def deliver_account_blocked(user)
-          admin_users = User.active.where(admin: true).to_a
-          admin_users.each do |admin_user|
-            account_blocked(admin_user, user).deliver_later
-          end
-        end
-      end
-
-      ##
-      # Injects InstanceMethods into base class.
-      #
-      module InstanceMethods
-        ##
-        # Informs admins only - avoid spam to other users
-        #
-        def account_blocked(admin, user)
-          @user = user
-          mail to: admin, subject: l('mailer.account_blocked_subject')
-        end
+  ##
+  # Injects ClassMethods into base class.
+  #
+  module ClassMethods
+    def deliver_account_blocked(user)
+      admin_users = User.active.where(admin: true).to_a
+      admin_users.each do |admin_user|
+        account_blocked(admin_user, user).deliver_later
       end
     end
+  end
+
+  ##
+  # Injects InstanceMethods into base class.
+  #
+  module InstanceMethods
+    ##
+    # Informs admins only - avoid spam to other users
+    #
+    def account_blocked(admin, user)
+      @user = user
+      mail to: admin, subject: l('mailer.account_blocked_subject')
+    end
+  end
 end
--- lib/redmine_login_attempts_limit/overrides/account_controller_patch.rb
+++ lib/redmine_login_attempts_limit/overrides/account_controller_patch.rb
@@ -4,76 +4,76 @@
 # Overrides methods of AccountController.
 #
 module RedmineLoginAttemptsLimit::Overrides::AccountControllerPatch
-      def self.prepended(base)
-        base.send(:prepend, InstanceMethods)
+  def self.prepended(base)
+    base.send(:prepend, InstanceMethods)
+  end
+
+  ##
+  # Instance methods to be prepended.
+  #
+  module InstanceMethods
+    include RedmineLoginAttemptsLimit::PluginSettings
+
+    # @override AccountController#password_authentication
+    def password_authentication
+      return super unless invalid_account.blocked?
+
+      # Log blocked authentication attempt with user id and remote IP
+      begin
+        uid = user&.id
+        Rails.logger.info "[redmine_login_attempts_limit] blocked authentication attempt user_id=#{uid.inspect} username=#{username.inspect} ip=#{request.remote_ip rescue 'unknown'}"
+      rescue StandardError => e
+        Rails.logger.error "[redmine_login_attempts_limit] failed to log blocked attempt: #{e.message}"
       end
 
-      ##
-      # Instance methods to be prepended.
-      #
-      module InstanceMethods
-        include RedmineLoginAttemptsLimit::PluginSettings
+      flash.now[:error] = l('errors.blocked')
+    end
 
-        # @override AccountController#password_authentication
-        def password_authentication
-          return super unless invalid_account.blocked?
+    # @override AccountController#invalid_credentials
+    def invalid_credentials
+      # Skip increment if already blocked
+      was_blocked = invalid_account.blocked?
+      invalid_account.update unless was_blocked
 
-          # Log blocked authentication attempt with user id and remote IP
-          begin
-            uid = user&.id
-            Rails.logger.info "[redmine_login_attempts_limit] blocked authentication attempt user_id=#{uid.inspect} username=#{username.inspect} ip=#{request.remote_ip rescue 'unknown'}"
-          rescue StandardError => e
-            Rails.logger.error "[redmine_login_attempts_limit] failed to log blocked attempt: #{e.message}"
-          end
+      super
+      return unless invalid_account.blocked?
 
-          flash.now[:error] = l('errors.blocked')
+      # Log locked user id when the account becomes blocked
+      begin
+        if !was_blocked && user.present?
+          Rails.logger.info "[redmine_login_attempts_limit] account locked for user_id=#{user.id}"
         end
-
-        # @override AccountController#invalid_credentials
-        def invalid_credentials
-          # Skip increment if already blocked
-          was_blocked = invalid_account.blocked?
-          invalid_account.update unless was_blocked
-
-          super
-          return unless invalid_account.blocked?
-
-          # Log locked user id when the account becomes blocked
-          begin
-            if !was_blocked && user.present?
-              Rails.logger.info "[redmine_login_attempts_limit] account locked for user_id=#{user.id}"
-            end
-          rescue StandardError => e
-            Rails.logger.error "[redmine_login_attempts_limit] failed to log locked user id: #{e.message}"
-          end
-
-          flash.now[:error] = l('errors.blocked')
-          Mailer.deliver_account_blocked(user) if notification? && user.present?
-        end
-
-        def successful_authentication(user)
-          invalid_account.clear(user.login)
-          super
-        end
-
-        def user
-          @user = User.find_by(login: username)
-        end
-
-        def invalid_account
-          @invalid_account ||= InvalidAccount.new(username)
-        end
-
-        def username
-          params[:username]
-        end
-
-        def token
-          Token.find_token('recovery', params[:token].to_s)
-        end
-
-        def notification?
-          setting['blocked_notification']
-        end
+      rescue StandardError => e
+        Rails.logger.error "[redmine_login_attempts_limit] failed to log locked user id: #{e.message}"
       end
+
+      flash.now[:error] = l('errors.blocked')
+      Mailer.deliver_account_blocked(user) if notification? && user.present?
+    end
+
+    def successful_authentication(user)
+      invalid_account.clear(user.login)
+      super
+    end
+
+    def user
+      @user = User.find_by(login: username)
+    end
+
+    def invalid_account
+      @invalid_account ||= InvalidAccount.new(username)
+    end
+
+    def username
+      params[:username]
+    end
+
+    def token
+      Token.find_token('recovery', params[:token].to_s)
+    end
+
+    def notification?
+      setting['blocked_notification']
+    end
+  end
 end
